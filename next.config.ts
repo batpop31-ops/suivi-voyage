ae0365f06e8d4a879d1a5ca51805269e1ce03391
batpop31-ops <batpop31@gmail.com>
2025-10-28 12:09:35 +0100

    Fix build Vercel: force Webpack

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,18 @@
 import withPWA from "next-pwa";
 import { NextConfig } from "next";
+
 const nextConfig: NextConfig = withPWA({
-reactStrictMode: true,
-experimental: {
-appDir: true,
-},
-pwa: {
-dest: "public",
-register: true,
-skipWaiting: true,
-},
-turbo: false, // utilise Webpack pour éviter les conflits avec next-pwa
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+  // ⚠ Forcer Webpack au lieu de Turbopack
+  turbopack: {}, // nécessaire pour Next 16 + Vercel
 });
+
 export default nextConfig;
